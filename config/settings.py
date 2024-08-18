a5e2aa72dce62280846743658caaac85701a17e9
Farukh <shutdownrunner@bk.ru>
2024-08-19 01:27:42 +0300

    выполнено задание 2

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
