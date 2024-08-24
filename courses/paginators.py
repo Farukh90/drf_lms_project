74eba65024cdf38206f4f839138e96400c15816f
Farukh <shutdownrunner@bk.ru>
2024-08-24 09:44:12 +0300

    скорректировано задания 3

--- courses/paginators.py
+++ courses/paginators.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class MyPaginator(PageNumberPagination):
-    page_size = 2
-    page_query_param = "page"
+    page_size = 3
+    page_size_query_param = "page_size"
     max_page_size = 7
