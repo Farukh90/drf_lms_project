24c53bf6fb8fd065597a5cdd96a41aec5b2cba9d
Farukh <shutdownrunner@bk.ru>
2024-08-24 12:28:54 +0300

    поправки задания 4

--- courses/tests.py
+++ courses/tests.py
@@ -49,7 +49,10 @@ class LessonTestCase(APITestCase):
             "description": "Описание",
             "video_url": "my_link.rutube.ru"
         }
-        self.assertRaises(ValidationError)
+
+        # Попытка создать урок с некорректными данными
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_lesson_update(self):
         url = reverse('courses:lessons_update', args=(self.course.pk, self.lesson.pk))
